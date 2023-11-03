be568080a1c3f984d25bd7624e4412b81ae2fd7b
Sid <saytleader@outlook.com>
2023-11-03 08:31:58 -0400

    Update Install.hx

--- src/functions/Install.hx
+++ src/functions/Install.hx
@@ -1,7 +1,13 @@
 package functions;
 
 class Install {
-    public static function copyFiles(pkg:String) {}
+    public static function copyFiles(pkg:String) {
+        final files = File.getContent('/tmp/escam/downloaded/$pkg/pkgfiles').split("\n");
+
+        for (file in files) {
+            Sys.command('cp /tmp/escam/downloaded/$pkg/$file $file');
+        }
+    }
 
     public static function updateDb(pkg:String) {}
 }
