2a74dfdeab31f62d9a902642405a3ee99b33f54e
Sid <saytleader@outlook.com>
2023-11-03 08:41:52 -0400

    Create Check.hx

---
+++ src/functions/Check.hx
@@ -0,0 +1,8 @@
+package functions;
+
+class Check {
+    // returns a list of conflicting pkgs
+    public static function pkgConflicts(pkg:String):Array<String> { return []; }
+
+    public static function fileConflicts(pkg:String):Array<String> { return []; }
+}
