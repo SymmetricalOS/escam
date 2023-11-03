2a24947bd960db58fde56dbd17141888e5b258a5
Sid <saytleader@outlook.com>
2023-11-03 13:53:49 -0400

    Update Check.hx

--- src/functions/Check.hx
+++ src/functions/Check.hx
@@ -2,7 +2,17 @@ package functions;
 
 class Check {
     // returns a list of conflicting pkgs
-    public static function pkgConflicts(pkg:String):Array<String> { return []; }
+    public static function pkgConflicts(pkg:String):Array<String> { 
+        var conflicts = [];
+        // TODO: get a list of pkginfos
+        final pkginfos = [];
+        for (pkginfo in pkginfos) {
+            if (pkginfo.conflicts.contains(pkg)) {
+                conflicts.push(pkginfo.name);
+            }
+        }
+        return conflicts;
+    }
 
     public static function fileConflicts(pkg:String):Array<String> { return []; }
 }
