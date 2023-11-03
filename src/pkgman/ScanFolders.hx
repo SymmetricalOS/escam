16978f7db9ee01c8a52bb535742fb164339fa369
Sid <saytleader@outlook.com>
2023-11-03 08:24:55 -0400

    Delete src/pkgman/ScanFolders.hx

--- src/pkgman/ScanFolders.hx
+++
@@ -1,7 +0,0 @@
-package pkgman;
-
-class ScanFolders {
-	public static function get():Array<String> {
-		return ["/usr/local/bin", "/usr/bin", "/bin", "/usr/local/sbin", "/usr/sbin", "/bin"];
-	}
-}
